dfad195defb31ce60c633bd5d834551f86f300f3
Raul U. M <raul.ugia@hotmail.com>
2025-01-13 13:10:14 +0100

    user entity created

--- src/main/java/com/filmhive/film_hive/entities/User.java
+++ src/main/java/com/filmhive/film_hive/entities/User.java
@@ -1,10 +1,120 @@
 package com.filmhive.film_hive.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 
 @Entity
 @Table(name ="user")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
 
+    @Column(name = "first_name")
+    @NotEmpty(message = "First name cannot be empty")
+    @NotNull(message = "First name is required")
+    @Size(min = 2, max = 20, message = "First name must be between 2 and 20 characters")
+    private String firstName;
+
+    @Column(name = "last_name")
+    @NotEmpty(message = "Last name cannot be empty")
+    @NotNull(message = "Last name is required")
+    @Size(min = 2, max = 20, message = "Last name must be between 2 and 20 characters")
+    private String lastName;
+
+    @Column(unique = true, name = "username")
+    @NotEmpty(message = "Username cannot be empty")
+    @NotNull(message = "Username is required")
+    @Size(min = 2, max = 20, message = "Username must be between 2 and 20 characters")
+    private String username;
+
+    @Email
+    @Column(name="email")
+    @NotEmpty(message = "Email cannot be empty")
+    @NotNull(message = "Email is required")
+    @Size(min = 5, max = 254, message = "Email must be between 5 and 254 characters")
+    private String email;
+
+    @Column(name = "password")
+    @NotEmpty(message = "Password cannot be empty")
+    @NotNull(message = "Password is required")
+    @Size(min = 6, max = 20, message = "Password must be between 6 and 20 characters")
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).+$",
+            message = "Password must contain at least one uppercase letter, one lowercase letter, and one number"
+    )
+    private String password;
+
+    public User(){
+
+    }
+
+    public User(String firstName, String lastName, String username, String email, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
